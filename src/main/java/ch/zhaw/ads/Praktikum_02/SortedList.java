94990fb8605f4cb28d5ca76dd2edc87962c2cd78
rhombtim <rhombtim@students.zhaw.ch>
2020-09-27 13:36:15 +0200

    Add fourth solution

---
+++ src/main/java/ch/zhaw/ads/Praktikum_02/SortedList.java
@@ -0,0 +1,36 @@
+package ch.zhaw.ads.Praktikum_02;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class SortedList extends MyList {
+    private ListNode head;
+    private int size = 0;
+
+    @Override
+    public boolean add(Object o) {
+        if (o != null) {
+            if (head == null) {
+                head = new ListNode(o);
+                head.setNextNode(head);
+                head.setPrevNode(head);
+
+            } else {
+                ListNode newNode = new ListNode(o);
+                ListNode searchNode = head;
+                ListNode lastNode = getLastNode();
+                lastNode.setNextNode(newNode);
+                newNode.setPrevNode(lastNode);
+                newNode.setNextNode(head);
+                head.setPrevNode(lastNode);
+            }
+            size++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
