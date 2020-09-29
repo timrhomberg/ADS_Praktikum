85c83e59d9b1fb274dfb9a14fd261a2f55b4eb66
rhombtim <rhombtim@students.zhaw.ch>
2020-09-29 18:53:36 +0200

    Add nine solution

--- src/main/java/ch/zhaw/ads/Praktikum_02/SortedList.java
+++ src/main/java/ch/zhaw/ads/Praktikum_02/SortedList.java
@@ -1,10 +1,5 @@
 package ch.zhaw.ads.Praktikum_02;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 public class SortedList<T extends Comparable<T>> extends MyList<T> {
     @Override
     public boolean add(T o) {
