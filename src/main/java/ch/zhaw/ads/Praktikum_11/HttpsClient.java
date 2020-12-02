225529c74e667b6a5352ad29b8f5d370e9172660
rhombtim <rhombtim@students.zhaw.ch>
2020-12-02 18:58:18 +0100

    add praktika 11

---
+++ src/main/java/ch/zhaw/ads/Praktikum_11/HttpsClient.java
@@ -0,0 +1,16 @@
+import java.net.MalformedURLException;
+import java.net.*;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class HttpsClient {
+
+    public static String getContentOfURL(String urlOfHomePage) {
+        String textOfHomePage = "";
+
+		// do to
+		return textOfHomePage;
+    }
+
+}
---
+++ src/main/java/ch/zhaw/ads/Praktikum_11/TelNrSearchServer.java
@@ -0,0 +1,18 @@
+import java.util.regex.*;
+
+public class TelNrSearchServer implements CommandExecutor {
+
+
+
+    //----- Dies implementiert das CommandExecutor Interface.
+    @Override
+    public String execute(String nameToSearch) {
+        String textOfHomePage;
+
+		// to do
+    }
+
+    private String extractPhoneNumbers(String textToSearch) {
+		// do do
+    }
+}
